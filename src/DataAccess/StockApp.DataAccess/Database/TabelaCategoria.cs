5cdbc1bb2ff949e6d8f096cbbd41c1b1a22a3aea
Welton Castoldi <weltoncastoldi@hotmail.com>
2023-12-12 20:44:13 -0300

    fix: Corrige errro de tabela existente.

--- src/DataAccess/StockApp.DataAccess/Database/TabelaCategoria.cs
+++ src/DataAccess/StockApp.DataAccess/Database/TabelaCategoria.cs
@@ -8,15 +8,17 @@ namespace StockApp.DataAccess.Database
         public void CriarTabelaCategoria()
         {
             var sql = @"
-                        CREATE TABLE Categorias
-                        (
-                          Id int PRIMARY KEY IDENTITY,
-                          Nome varchar(100),
-                          Status bit,
-                          DataCriacao date,
-                          DataAlteracao date,
-                        )
-                       ";
+                    IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Categorias')
+                    BEGIN
+                            CREATE TABLE Categorias
+                            (
+                              Id int PRIMARY KEY IDENTITY,
+                              Nome varchar(100),
+                              Status bit,
+                              DataCriacao date,
+                              DataAlteracao date,
+                            );
+                    END;";
             try
             {
                 using (var conexao = new SqlConnection(SqlServerContext.ConexaoComBanco))
